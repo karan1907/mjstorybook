a8625c1c3f876ab04b0e49546d91b7a5358bbd71
Goldy King <goldyking@Goldys-MacBook-Pro.local>
2019-07-26 16:03:52 +0530

    Started Ui Work

--- app.js
+++ app.js
@@ -1,25 +1,72 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const passport = require('passport')
+const keys = require('./config/keys')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+// ========= load User Model ==========
+require('./models/User')
+
+// ========= Handlebars Middleware ======
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}))
+app.set('view engine', 'handlebars')
+
+
+//========== Cookie and express-session ========
+app.use(cookieParser())
+app.use(session({
+    secret: 'Goldy King',
+    resave: false,
+    saveUninitialized: false
+  }))
 
 //======== Passport Config ========
 require('./config/passport')(passport)
 
+// ======= Passport Middleware ======
 
-const app = express()
+app.use(passport.initialize())
+app.use(passport.session())
+
+// ========= Set Global Variable ========
+
+app.use((req,res,next) => {
+    res.locals.user = req.user || null
+    next()
+})
+
+// =========== Mongoose Connect =====
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('MongoDB Connected')
+}).catch((error) => {
+    console.log(error)
+})
+
+
+
 
 const port = process.env.PORT || 3000
 
 
 // =========== Load Routes ===========
+const index = require('./routes/index')
 const auth = require('./routes/auth')
 
-app.get('/', (req,res) => {
-    res.send('It Works!')
-})
+
 
 // ========== Use Routes =============
 
+app.use('/', index)
 app.use('/auth', auth)
+
 
 app.listen(port, () => console.log(`Server is running at port ${port}`))
--- config/passport.js
+++ config/passport.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 const keys = require('./keys')
 
 
+// =========== Load User Model ==========
+const User = mongoose.model('users')
+
 module.exports = function(passport){
     passport.use(new GoogleStrategy({
         clientID: keys.googleClientID,
@@ -10,8 +13,38 @@ module.exports = function(passport){
         callbackURL: '/auth/google/callback',
         proxy: true
     }, (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken)
-        console.log(profile)
+        // console.log(accessToken)
+        //console.log(profile)
+
+        const newUser = {
+            googleID: profile.id,
+            firstname: profile.name.givenName,
+            lastname: profile.name.familyName,
+            email: profile.emails[0].value,
+            image: profile.photos[0].value
+        }
+
+        //========= Check For Existing User ===========
+        User.findOne({
+            googleID: profile.id
+        }).then(user => {
+            if(user){
+                // Return User
+                done(null, user)
+            }else{
+                // Create User
+                new User(newUser).save()
+                .then(user => done(null, user))
+            }
+        })
+
     })
    )
+   passport.serializeUser((user, done) => {
+       done(null, user.id)
+   })
+
+   passport.deserializeUser((id, done) => {
+        User.findById(id).then(user => done(null, user))
+    })
 }
