70a1f9980cb291e952ad382966f0c5a8e92c7adb
Goldy King <goldyking@Goldys-MacBook-Pro.local>
2019-07-26 09:31:29 +0530

    Handled Auth

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const passport = require('passport')
+
+//======== Passport Config ========
+require('./config/passport')(passport)
+
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
+
+// =========== Load Routes ===========
+const auth = require('./routes/auth')
+
 app.get('/', (req,res) => {
     res.send('It Works!')
 })
+
+// ========== Use Routes =============
+
+app.use('/auth', auth)
 
 app.listen(port, () => console.log(`Server is running at port ${port}`))
